11dde833fe99551886692aaf152087c61397fd47
Mauricio <mauricio.eduardo.nr@gmail.com>
2025-10-24 22:59:07 +0200

    renamed variable

--- utils/corpus_metrics.py
+++ utils/corpus_metrics.py
@@ -3,19 +3,21 @@ from abstract_class.base_statistics import BaseStatistics
 
 class CorpusMetrics(BaseStatistics):
     def __init__(self, word_count, vocab_analyzer):
-        self.word_counter = word_count
+        self.word_analysis = word_count
         self.vocab_analyzer = vocab_analyzer
 
     def calculate(self, df):
-        word_counts = self.word_counter.calculate_word_count(df, "clean_text")
-        total_words = self.word_counter.calculate_total_words(word_counts)
+        word_counts = self.word_analysis.calculate_word_count(df, "clean_text")
+        total_words = self.word_analysis.calculate_total_words(word_counts)
 
-        word_counts_no_stop = self.word_counter.calculate_word_count(df, "no_stopwords")
-        total_no_stopwords = self.word_counter.calculate_total_words(
+        word_counts_no_stop = self.word_analysis.calculate_word_count(
+            df, "no_stopwords"
+        )
+        total_no_stopwords = self.word_analysis.calculate_total_words(
             word_counts_no_stop
         )
 
-        base_stats = self.word_counter.calculate_statistics(word_counts, total_words)
+        base_stats = self.word_analysis.calculate_statistics(word_counts, total_words)
         vocab_size = self.vocab_analyzer.calculate_vocab_size(df, "clean_text")
 
         percentage_stop_words = (total_no_stopwords * 100) / total_words
